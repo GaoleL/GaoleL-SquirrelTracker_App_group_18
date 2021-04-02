5c69ae49e98a341f428c8456f56af6bfad6e1bdc
gl2704 <gl2704@columbia.edu>
2021-04-02 06:09:01 +0000

    add enviornment varibals

--- Squirrel_Tracker/settings.py
+++ Squirrel_Tracker/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,8 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'w(-^6wp+x_!_#s9(^2eew_72suj9$90^96bbp#m@3we4u!(z%*'
 
+SECRET_KEY = 'w(-^6wp+x_!_#s9(^2eew_72suj9$90^96bbp#m@3we4u!(z%*' 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
