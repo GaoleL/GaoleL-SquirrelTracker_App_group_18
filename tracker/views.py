669efb3921b4f831953ff89d9f8e6c23dbe56621
gl2704 <gl2704@columbia.edu>
2021-04-02 20:38:01 +0000

    finsh part of view.py, home and sightings page

--- tracker/views.py
+++ tracker/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.http import HttpResponse
+from .models import squirrel
+
 
 # Create your views here.
+def home(request):
+    return render(request,'tracker/home.html')
+
+
+def all_sightings(request):
+    squirrels = squirrels.objects.all()
+    context = {
+            'squirrels':squirrels
+            }
+    return render(request,'tracker/all.html',context)
+
+
+
