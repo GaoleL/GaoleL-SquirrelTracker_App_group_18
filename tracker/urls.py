b8d64747de678a907b5306c8af1a156770e109f1
gl2704 <gl2704@columbia.edu>
2021-04-03 23:40:01 +0000

    Finish sightings/add

--- tracker/urls.py
+++ tracker/urls.py
@@ -5,6 +5,7 @@ from django.conf.urls import url
 urlpatterns=[
 		path('',views.home),
         path('sightings/',views.all_sightings),
-        
+        path('sightings/add/',views.add,name='add'),
+
         
         ]
--- tracker/views.py
+++ tracker/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from .models import squirrel
-
+from .forms import Form
 
 # Create your views here.
 def home(request):
@@ -15,5 +15,13 @@ def all_sightings(request):
             }
     return render(request,'tracker/all.html',context)
 
-
-
+def add(request):
+    if request.method == 'POST':
+        form = Form(request.POST)
+        if form.is_valid():
+            form.save()
+        return redirect('/tracker/')
+    else:
+        form = Form()
+        context = {'form':form,}
+        return render(request,'tracker/form.html',context)
