9dedcf3b98617cc9c01937a1429fe601a55cdd59
gl2704 <gl2704@columbia.edu>
2021-04-04 02:27:07 +0000

    Finish squirrel data import

--- tracker/management/commands/import_squirrel_data.py
+++ tracker/management/commands/import_squirrel_data.py
@@ -2,6 +2,7 @@ from django.core.management.base import BaseCommand
 from tracker.models import squirrel
 import csv
 import datetime
+from distutils.util import strtobool
 class Command(BaseCommand):
     help='This command is used for Import squirrel data from csv file'
     def add_arguments(self,parser):
@@ -13,7 +14,7 @@ class Command(BaseCommand):
             with open(path,encoding='utf-8') as f:
                 reader = csv.DictReader(f)
                 for i in reader:
-                    s = Squirrels(
+                    s = squirrel(
                         X=i['X'],
                         Y=i['Y'],
                         Unique_squirrel_id=i['Unique Squirrel ID'],
@@ -23,20 +24,21 @@ class Command(BaseCommand):
                         Primary_Fur_Color=i['Primary Fur Color'].lower(),
                         Location = i['Location'],
                         Specific_location = i['Specific Location'],
-                        Running=i['Running'].lower(),
-                        Chasing=i['Chasing'].lower(),
-                        Climbing=i['Climbing'].lower(),
-                        Eating=i['Eating'].upper(),
-                        Foraging=i['Foraging'].upper(),
-                        Other_activities=i['Other Activities'].lower(),
-                        Kuks=i['Kuks'].upper(),
-                        Quaas=i['Quaas'].upper(),
-                        Moans=i['Moans'].upper(),
-                        Tail_flags=i['Tail flags'].upper(),
-                        Tail_twitches=i['Tail twitches'].upper(),
-                        Approaches=i['Approaches'].upper(),
-                        Indifferent=i['Indifferent'].upper(),
-                        Runs_from=i['Runs from'].upper(),
+                        
+                        Running=strtobool(i['Running']),
+                        Chasing=strtobool(i['Chasing']),
+                        Climbing=strtobool(i['Climbing']),
+                        Eating=strtobool(i['Eating']),
+                        Foraging=strtobool(i['Foraging']),
+                        Other_activities=i['Other Activities'],
+                        Kuks=strtobool(i['Kuks']),
+                        Quaas=strtobool(i['Quaas']),
+                        Moans=strtobool(i['Moans']),
+                        Tail_flags=strtobool(i['Tail flags']),
+                        Tail_twitches=strtobool(i['Tail twitches']),
+                        Approaches=strtobool(i['Approaches']),
+                        Indifferent=strtobool(i['Indifferent']),
+                        Runs_From=strtobool(i['Runs from']),
                         )
                     s.save()
         except csv.Error as e:
